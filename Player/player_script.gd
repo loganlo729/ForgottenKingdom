91d3c38c31a7b05308db27224b58483c3e091d22
loganlo729 <loganlo729@gmail.com>
2026-09-22 11:58:37 -0400

    Fine-tuned running/jumping

--- Player/player_script.gd
+++ Player/player_script.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 const SPEED = 150.0
-const RUN_SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const RUN_SPEED = 250.0
+const JUMP_VELOCITY = -380.0
 
 @onready var sprite = $AnimatedSprite2D
 
@@ -17,7 +17,7 @@ func _physics_process(delta):
 
 	if direction:
 		velocity.x = direction * SPEED
-		if Input.is_action_pressed("run") and is_on_floor():
+		if Input.is_action_pressed("run"):
 			velocity.x = direction * RUN_SPEED
 		if direction < 0:
 			sprite.flip_h = true
